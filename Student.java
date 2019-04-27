67a50af1ded3332d8dedf4098ff7f0cc749cafdd
nguyensouma <48306559+nguyensouma@users.noreply.github.com>
2019-04-27 10:48:14 +0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,46 +1,43 @@
 package ADF2_Exam;
 public class Student {
-    public int id;
-    public String name;
-    public String address;
-    public String phone;
+    String studentID;
+    String name;
+    String address;
+    String phone;
 
-    public Student(int id, String name, String address, String phone) {
-        this.id = id;
+    public Student(String studentID, String name, String address, String phone) {
+        this.studentID = studentID;
         this.name = name;
         this.address = address;
         this.phone = phone;
     }
 
-    public Student() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+    public String getStudentID() {
+        return studentID;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getAddress() {
         return address;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public String getPhone() {
         return phone;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public void setPhone(String phone) {
